10e07b109dd2e8ed32dbb62c76f173064699682b
Sanaalagh <Sanaalaghzou@gmail.com>
2024-04-28 15:51:40 -0700

    some changes

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -7,6 +7,12 @@ and other basic API operations.
 from api.v1.views import app_views
 from flask import jsonify
 from models import storage
+from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
 
 
 @app_views.route('/status', methods=['GET'])
